0aea6a7dd9fce20daab809bd5c973677aa9acc5e
MatheusFerrazDev <contatomatheus.oferraz@gmail.com>
2026-02-27 10:20:05 -0300

    corração arquivo mail.js

--- src/lib/mail.js
+++ src/lib/mail.js
@@ -1,21 +1,20 @@
 import nodemailer from "nodemailer";
 
+import nodemailer from "nodemailer";
+
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true para porta 465 (SSL)
+  port: 587,
+  secure: false,
+  family: 4, // Tenta forçar IPv4 para evitar bloqueio do Gmail
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Lembre-se: Senha de App de 16 dígitos
+    pass: process.env.EMAIL_PASS,
   },
   tls: {
-    // Essencial para evitar falhas de handshake em servidores Linux/Docker
     rejectUnauthorized: false,
   },
-  // Define tempos curtos para o servidor não ficar travado se o Gmail demorar
-  connectionTimeout: 10000, // 10 segundos
-  greetingTimeout: 5000,
-  socketTimeout: 15000,
+  connectionTimeout: 15000, // Aumentei um pouco para dar fôlego ao servidor
 });
 
 // Teste de conexão
